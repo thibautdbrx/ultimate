563710ce844e7053d33dbbb4b76af763811a18b8
thibautdbrx <105484429+thibautdbrx@users.noreply.github.com>
2025-11-02 11:47:22 +0100

    init

---
+++ backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/ApiUltimateApplication.java
@@ -0,0 +1,13 @@
+package org.ultimateam.apiultimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUltimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUltimateApplication.class, args);
+	}
+
+}
