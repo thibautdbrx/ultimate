74af8544b18d0c6ef6b117dff4f2fcd32d1185c8
thibautdbrx <105484429+thibautdbrx@users.noreply.github.com>
2025-11-03 22:44:41 +0100

    Fix:Gestion match Post

--- backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/controller/MatchController.java
+++ backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/controller/MatchController.java
@@ -1,7 +1,9 @@
 package org.ultimateam.apiultimate.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.ultimateam.apiultimate.model.Equipe;
 import org.ultimateam.apiultimate.model.Match;
+import org.ultimateam.apiultimate.service.EquipeService;
 import org.ultimateam.apiultimate.service.MatchService;
 
 import java.util.List;
@@ -10,9 +12,13 @@ import java.util.List;
 @RequestMapping("/api/match")
 public class MatchController {
 
-    private MatchService matchService;
+    private final MatchService matchService;
+    private EquipeService equipeService;
 
-    public MatchController(MatchService matchService) { this.matchService = matchService; }
+    public MatchController(MatchService matchService, EquipeService equipeService) {
+        this.matchService = matchService;
+        this.equipeService = equipeService;
+    }
 
     @GetMapping
     public List<Match> getAllMatch() { return (List<Match>) matchService.getAll();}
@@ -21,7 +27,18 @@ public class MatchController {
     public Match getById(@PathVariable Long id) { return matchService.getById(id);}
 
     @PostMapping
-    public Match createMatch(@RequestBody Match match) { return matchService.save(match);}
+    public Match createMatch(@RequestBody Match match) {
+
+        //Récupère les équipes grâce aux id des équipes
+        Equipe equipe1 = equipeService.getById(match.getEquipe1().getId_equipe());
+        Equipe equipe2 = equipeService.getById(match.getEquipe2().getId_equipe());
+
+        //Associe les équipes grâce à leurs id
+        match.setEquipe1(equipe1);
+        match.setEquipe2(equipe2);
+
+        return matchService.save(match);
+    }
 
     @DeleteMapping("/{id}")
     public void deleteMatch(@PathVariable Long id) { matchService.deleteById(id);}
--- backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/model/Match.java
+++ backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/model/Match.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -21,12 +22,10 @@ public class Match {
 
     @ManyToOne
     @JoinColumn(name = "id_equipe1")
-    @JsonBackReference("equipe1-matches")
     private Equipe equipe1;
 
     @ManyToOne
     @JoinColumn(name = "id_equipe2")
-    @JsonBackReference("equipe2-matches")
     private Equipe equipe2;
 
     /**
@@ -52,5 +51,43 @@ public class Match {
         this.status = Status.WAITING;
         this.score_equipe1 = 0;
         this.score_equipe2 = 0;
+    }
+
+    public void ajouterPoint(Equipe equipe) {
+        if (this.status == Status.FINISHED) {
+            throw new IllegalStateException("Impossible de retirer un point, le match est terminé");
+        }
+        if (Objects.equals(equipe.getId_equipe(), this.equipe1.getId_equipe())) {
+            score_equipe1++;
+        } else if (Objects.equals(equipe.getId_equipe(), this.equipe2.getId_equipe())) {
+            score_equipe2++;
+        } else {
+            throw new IllegalArgumentException("Cette équipe ne fait pas partie du match");
+        }
+    }
+
+    public void retirerPoint(Equipe equipe) {
+        if (this.status == Status.FINISHED) {
+            throw new IllegalStateException("Impossible de retirer un point, le match est terminé");
+        }
+        if (Objects.equals(equipe.getId_equipe(), this.equipe1.getId_equipe())) {
+            score_equipe1--;
+        }
+        if (Objects.equals(equipe.getId_equipe(), this.equipe2.getId_equipe())) {
+            score_equipe2--;
+        }
+        else {
+            throw new IllegalArgumentException("Cette équipe ne fait pas partie du match");
+        }
+    }
+
+    public void finirMatch() {
+        this.date_fin = LocalDateTime.now();
+        this.status = Status.FINISHED;
+
+    }
+    public void commencerMatch() {
+        this.date_debut = LocalDateTime.now();
+        this.status = Status.ONGOING;
     }
 }
