e56732bdb6c28184b201c62e3522327ad0befa1b
DEBREUX Thibaut <thibaut.debreux7@etu.univ-lorraine.fr>
2025-11-29 15:51:28 +0100

    feat:ajout controller classement pour swagger

--- backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/controller/ClassementController.java
+++ backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/controller/ClassementController.java
@@ -1,7 +1,22 @@
 package org.ultimateam.apiultimate.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ultimateam.apiultimate.service.ClassementService;
 
 @Tag(name = "Classement", description = "Endpoints pour gérer le classement")
+@RestController
+@RequestMapping("/api/classement")
 public class ClassementController {
+
+    private final ClassementService classementService;
+    public ClassementController(ClassementService classementService) {
+        this.classementService = classementService;
+    }
+    @GetMapping
+    public void getClassement(){
+        classementService.afficherClassement();
+    }
 }
