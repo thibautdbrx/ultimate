fb757e604018c46da5acb946734aa4d86954b30d
DEBREUX Thibaut <thibaut.debreux7@etu.univ-lorraine.fr>
2025-11-30 12:52:06 +0100

    feat:upload images

--- backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/ApiUltimateApplication.java
+++ backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/ApiUltimateApplication.java
@@ -2,8 +2,11 @@ package org.ultimateam.apiultimate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.ultimateam.apiultimate.configuration.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ApiUltimateApplication {
 
 	public static void main(String[] args) {
