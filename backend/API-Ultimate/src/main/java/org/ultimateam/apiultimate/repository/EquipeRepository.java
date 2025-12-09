48d062609951465517af4db153b3f29c0cb87682
DEBREUX Thibaut <thibaut.debreux7@etu.univ-lorraine.fr>
2025-12-09 21:43:51 +0100

    feat:tri des équipes par genre

--- backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/repository/EquipeRepository.java
+++ backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/repository/EquipeRepository.java
@@ -3,5 +3,8 @@ package org.ultimateam.apiultimate.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ultimateam.apiultimate.model.Equipe;
 
+import java.util.List;
+
 public interface EquipeRepository extends JpaRepository<Equipe, Long> {
+    List<Equipe> findAllByGenre(Equipe.Genre genre);
 }
