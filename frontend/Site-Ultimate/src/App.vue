1669a785b9cb0e60f358dcd63f11fabcaca0627c
loup <e45497u@etu.univ-lorraine.fr>
2026-01-21 20:01:52 +0100

    steky footer

--- frontend/Site-Ultimate/src/App.vue
+++ frontend/Site-Ultimate/src/App.vue
@@ -1,19 +1,31 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
-
 </script>
 
 <template>
   <div id="app">
     <Header />
-    <router-view /> <!-- C’est ici que la page actuelle s’affiche -->
-  <Footer />
+    <main>
+      <router-view />
+    </main>
+    <Footer />
   </div>
 </template>
 
 <style>
+body {
+  margin: 0;
+}
 
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
 
+main {
+  flex: 1;
+}
 </style>
