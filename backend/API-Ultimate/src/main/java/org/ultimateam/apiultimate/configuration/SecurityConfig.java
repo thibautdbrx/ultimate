17b2da832162fcb80ddd6bc03d3761113347bc20
DEBREUX Thibaut <thibaut.debreux7@etu.univ-lorraine.fr>
2026-01-21 15:33:45 +0100

    fix:page request

--- backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/configuration/SecurityConfig.java
+++ backend/API-Ultimate/src/main/java/org/ultimateam/apiultimate/configuration/SecurityConfig.java
@@ -91,9 +91,9 @@ public class SecurityConfig {
                         // 3. JOUEUR CONNECTÉ (Actions courantes)
                         // Upload image, rejoindre équipe, modifier son profil (PATCH)
                         .requestMatchers("/api/files/upload").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/api/joueur/request/**").authenticated()
-                        .requestMatchers(HttpMethod.PATCH, "/api/joueur/**").authenticated()
-                        .requestMatchers(HttpMethod.PATCH, "/api/joueur/request/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/joueur/request/**").hasAuthority("ROLE_VISITEUR")
+                        .requestMatchers(HttpMethod.PATCH, "/api/joueur/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_VISITEUR")
+                        .requestMatchers(HttpMethod.PATCH, "/api/joueur/request/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_VISITEUR")
 
                         // 4. ARBITRE & ADMIN (Gestion Matchs)
                         .requestMatchers(
